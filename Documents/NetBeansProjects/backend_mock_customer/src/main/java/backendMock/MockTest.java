c4756bfd88355208160b8f7a9be7e41829ec3094
Madalina <cristina.madalina.dragan@gmail.com>
2016-11-13 14:31:06 +0100

    changed the version of the contract jar file in the pom file made the dummy backend to implement the customer interface from the contract project

--- Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/MockTest.java
+++ Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/MockTest.java
@@ -13,23 +13,26 @@ public class MockTest {
     private static ReservationDetailListManagement reservationDetailListManagement = new ReservationDetailListManagement();
     private static DepartureIdentifier departureId = new DepartureIdentifier( 1 );
     private static ReservationIdentifier reservationIdentifier = new ReservationIdentifier( 1 );
+    private static ReservationIdentifier reservationIdentifier2 = new ReservationIdentifier( 2 );
 
     public static void main( String[] args ) {
         //tests the seeReservation method
-        System.out.println( dummyCustomerBackend.seeReservation( 1 ).getCustomerName() );
+        System.out.println( dummyCustomerBackend.getReservation( reservationIdentifier ).getCustomerName() );
         
         //tests the saveReservation method
-        dummyCustomerBackend.saveReservation( departureId, 14, "small" );
-        System.out.println( dummyCustomerBackend.seeReservation( 2 ).getNumberOfPeople());
+        dummyCustomerBackend.saveReservation( departureId, 14, 14, true,  1, 0);
+        System.out.println( dummyCustomerBackend.getReservation( reservationIdentifier2 ).getNumberOfPeople());
         
         //tests the updateReservation method
-        dummyCustomerBackend.updateReservation( reservationIdentifier, departureId, 25, "lorry" );
-        System.out.println( dummyCustomerBackend.seeReservation( 1 ).getNumberOfPeople() );
+        dummyCustomerBackend.updateReservation( reservationIdentifier, departureId, 4, 0, true );
+        System.out.println( dummyCustomerBackend.getReservation( reservationIdentifier ).getNumberOfPeople() );
         
         //tests the deleteReservation method
-        System.out.println( reservationDetailListManagement.getReservationDetails().values() );
+        System.out.println( "Initially, the list size is " 
+                + reservationDetailListManagement.getReservationDetails().size());
         dummyCustomerBackend.deleteReservation( reservationIdentifier );
-        System.out.println( reservationDetailListManagement.getReservationDetails().values() );
+        System.out.println( "After deletion, the list size is " 
+                + reservationDetailListManagement.getReservationDetails().size());
     }
 
 }
