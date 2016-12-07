2b4e831ae1c8069da0afd59f5f12c74395f783d6
nmcristian <nmcristian@gmail.com>
2016-12-07 11:23:12 +0100

    made saveReservation use provided customerName

--- Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/DummyCustomerBackend.java
+++ Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/DummyCustomerBackend.java
@@ -95,7 +95,7 @@ public class DummyCustomerBackend implements CustomerInterface { //should implem
                 + depDetail.getPricePerResident() * numberOfResidents;
         DepartureSummary depSummary = departureDetailListManagement.getDepartures().get(departureIdentifier.getId());
         ReservationDetail newReservationDetail = new ReservationDetail(depSummary.getDepartureTime(), depSummary,
-                "Mark Johnson", passengersNb, numberOfResidents,
+                customerName, passengersNb, numberOfResidents,
                 carsNumber, numberOfLorries, numberOfHeavyMachinery,
                 price, Math.toIntExact(reservationDetailListManagement.getNextIdReservationDetail()));
         reservationDetailListManagement.addReservationDetail(newReservationDetail);
